2352b1899b2acb655fbed764c7bc1b6e5dbb612f
Siberiaaa <siberiamvg@gmail.com>
2023-10-26 22:45:28 -0400

    bendita promise

--- main.js
+++ main.js
@@ -1,22 +1,3 @@
-/*
-async function test() {
-    const response = await fetch(
-        "https://sa-east-1.aws.data.mongodb-api.com/app/musicapp-clxou/endpoint/song",
-        {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({
-                name: "Snow",
-                author: "Red Hot Chilli Peppers",
-                status: "a",
-            }),
-        }
-    );
-
-    const data = await response.json();
-    console.log(data);
-}*/
-
 import * as footer from './modules/footer.js';
 import * as header from './modules/header.js';
 
